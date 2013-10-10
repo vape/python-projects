46a5f06a97c433066d298e9962cb42a783ca23a1
Taylan Aydinli <taylanaydinli@gmail.com>
2013-10-10 21:51:33 +0300

    Finished PI to Nth Digit project
    Used a spigot algorithm which keeps pumping
    out digits until you break the loop.

--- Numbers/01_Pi/pi.py
+++ Numbers/01_Pi/pi.py
@@ -1,27 +1,63 @@
-from math import sqrt
-from decimal import Decimal, getcontext
-getcontext().prec = 50
+from time import time
 
-def gauss_legendre():
-    a = [Decimal(1)]
-    b = [Decimal(1)/Decimal(sqrt(2))]
-    t = [Decimal(0.25)]
-    p = [Decimal(1)]
 
-    for i in range(0, 20):
-        n = i
-        a.append(Decimal((a[n] + b[n])) / Decimal(2))
-        b.append(Decimal(sqrt(a[n] * b[n])))
-        t.append(t[n] - (p[n] * ((a[n] - a[n+1])**2)))
-        p.append(p[n] * 2)
+def pi_spigot():
+    """
+    Generator function based on Jeremy Gibbons's spigot algorithm
+    [http://www.cs.ox.ac.uk/people/jeremy.gibbons/publications/spigot.pdf]
+    Yields digits of pi indefinitely.
+    """
+    q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+    while True:
+        if 4 * q + r - t < n * t:
+            yield n
+            nr = 10 * (r - n * t)
+            n = ((10 * (3 * q + r)) // t) - 10 * n
+            q *= 10
+            r = nr
+        else:
+            nr = (2 * q + r) * l
+            nn = (q * (7 * k) + 2 + (r * l)) // (t * l)
+            q *= k
+            t *= l
+            l += 2
+            k += 1
+            n = nn
+            r = nr
 
-        pi = ((a[n+1] + b[n+1])**2) / (4 * t[n+1])
-        print(format(pi, '.50f'))
+
+def calculate_pi(num_digits):
+    start = time()
+    digits = []
+    current_digit = 0
+    for d in pi_spigot():
+        current_digit += 1
+        digits.append(str(d))
+        if current_digit > num_digits:
+            break
+
+    end = time()
+    return ''.join(digits[1:]), (end - start)
+
+
+def print_in_blocks(lst, size):
+    num_blocks = int(len(lst) / size) + 1
+    for i in range(0, num_blocks):
+        print(''.join(lst[i*size:(i*size)+size]))
 
 
 def main():
-    gauss_legendre()
-    pass
+    try:
+        num_digits = min(30000, int(input("Please enter the number of digits [1, 30000]: ")))
+        print('Calculating {0} digits of PI'.format(num_digits))
+    except Exception as e:
+        print(e)
+        exit(1)
+
+    pi_digits, elapsed = calculate_pi(num_digits)
+    print('Here is {0} digits of PI calculated in {1:.3f} seconds.'.format(num_digits, elapsed))
+    print('3.')
+    print_in_blocks(pi_digits, 40)
 
 
 if __name__ == "__main__":
