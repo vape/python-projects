96394825437c84e1eb1f7d897b29fed0452f6c21
Taylan Aydinli <taylanaydinli@gmail.com>
2013-10-11 19:34:29 +0300

    Added iterative solution to Fibonacci project
    The iterative solution obviously doesn't suffer from
    recursion limits. It also uses next to no memory.

--- Numbers/02_Fibonacci/fibonacci.py
+++ Numbers/02_Fibonacci/fibonacci.py
@@ -23,15 +23,27 @@ def fib_recursive(num):
     else:
         return fib_recursive(num - 1) + fib_recursive(num - 2)
 
-#def fib_iterative(num):
+
+def fib_iterative(num):
+    if num in (0, 1):
+        return num
+
+    x1, x2, xn = 0, 1, 0
+    for i in range(2, num + 1):
+        xn = x1 + x2
+        x1, x2 = x2, xn
+
+    return xn
+
 
 def main():
     try:
-        target = min(10000, int(input('Enter target number [0, 1500]: ')))
+        target = int(input('Enter target number: '))
+        fib_func = fib_recursive if 0 <= target <= 1000 else fib_iterative
         start = time()
-        res = fib_recursive(target)
+        res = fib_func(target)
         end = time()
-        print('Fib({0}) [Recursive]: {1}. Calculated in {2:.3f} seconds.'.format(target, res, (end - start)))
+        print('Fib({0}) [{1}]: {2}. Calculated in {3:.3f} seconds.'.format(target, fib_func.__name__, res, (end - start)))
     except ValueError:
         print('Invalid input.')
         exit(1)
