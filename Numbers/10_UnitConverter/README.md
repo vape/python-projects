74c4e18d1cbc2a348ef8070f425513f84d79dd2f
Taylan Aydinli <taylanaydinli@gmail.com>
2013-10-23 20:24:48 +0300

    Added solution for Unit Converter project
    Just a basic implementation for now. I'll add more
    units and refactor the code.

---
+++ Numbers/10_UnitConverter/README.md
@@ -0,0 +1,5 @@
+### Unit Converter (temp, currency, volume, mass and more)
+
+Converts various units between one another. The user enters the type of unit
+being entered, the type of unit they want to convert to and then the value. The
+program will then make the conversion.
---
+++ Numbers/10_UnitConverter/converter.py
@@ -0,0 +1,100 @@
+class UnsupportedUnitException(BaseException):
+    def __init__(self, unit):
+        self.unit = unit
+
+    @property
+    def unit(self):
+        return self.unit
+
+class DifferentUnitsException(BaseException):
+    def __init__(self, cat1, cat2):
+        self._cat1 = cat1
+        self._cat2 = cat2
+
+categories = [
+    {
+        'name': 'mass',
+        'base_unit': 'kg',
+        'units': [
+            {
+                'name': 'kg',
+                'val_in_base': 1
+            },
+            {
+                'name': 'g',
+                'val_in_base': 0.001
+            },
+            {
+                'name': 'mg',
+                'val_in_base': 0.000001
+            }
+        ]
+    },
+    {
+        'name': 'length',
+        'base_unit': 'm',
+        'units': [
+            {
+                'name': 'm',
+                'val_in_base': 1
+            },
+            {
+                'name': 'cm',
+                'val_in_base': 0.01
+            },
+            {
+                'name': 'mm',
+                'val_in_base': 0.001
+            },
+            {
+                'name': 'inch',
+                'val_in_base': 0.0254
+            },
+            {
+                'name': 'km',
+                'val_in_base': 1000
+            }
+        ]
+    }
+]
+
+
+def find_category(unit):
+    for c in categories:
+        if [u for u in c['units'] if u['name'] == unit]:
+            return c
+    return ''
+
+
+def get_unit(category, unit):
+    return [u for u in category['units'] if u['name'] == unit][0]
+
+def convert_value(value, src_unit_name, target_unit_name):
+    src_cat, target_cat = find_category(src_unit_name), find_category(target_unit_name)
+    if src_cat != target_cat:
+        raise DifferentUnitsException(src_cat, target_cat)
+
+    src_unit = get_unit(src_cat, src_unit_name)
+    target_unit = get_unit(src_cat, target_unit_name)
+
+    value_in_base_unit = value * src_unit['val_in_base']
+
+    return value_in_base_unit / target_unit['val_in_base']
+
+
+def main():
+    cat_names = [c['name'] for c in categories]
+    print('Hello! With this calculator you can convert units of {0}.'.format(', '.join(cat_names)))
+    src_val, src_unit, target_unit = input('Please enter source value, source unit and unit to convert to [e.g. 100 m km]:').split(' ')
+    src_val = float(src_val)
+
+    try:
+        converted = convert_value(src_val, src_unit, target_unit)
+        print('{0} {1} = {2:.2f} {3}'.format(src_val, src_unit, converted, target_unit))
+    except UnsupportedUnitException as uue:
+        print('Sorry. I don\'t know what {0} is.'.format(uue.unit))
+    except DifferentUnitsException as due:
+        print('Sorry. These two units belong to different categories ({0} and {1}) and cannot be converted.'.format(due._cat1, due._cat2))
+
+if __name__ == '__main__':
+    main()
