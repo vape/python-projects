f7c0cc710665d547933ec131ad0b5f62a8b38cac
Taylan Aydinli <taylanaydinli@gmail.com>
2013-11-10 17:22:35 +0200

    Added BubbleSort implementation to Sorting project

--- Algorithms/sorting/sorting.py
+++ Algorithms/sorting/sorting.py
@@ -15,13 +15,31 @@ def _merge_lists(l1, l2):
     return nl
 
 
-def merge_sort_recursive(l):
-    return l if len(l) <= 1 else _merge_lists(merge_sort_recursive(l[0:len(l) // 2]),
-                                              merge_sort_recursive(l[len(l) // 2:]))
+def merge_sort(l):
+    return l if len(l) <= 1 else _merge_lists(merge_sort(l[0:len(l) // 2]),
+                                              merge_sort(l[len(l) // 2:]))
+
+
+def bubble_sort(l):
+    for i in range(0, len(l)):
+        i1, i2, swap = 0, 1, False
+        while i2 < len(l) - i:
+            if l[i1] > l[i2]:
+                swap = True
+                l[i1], l[i2] = l[i2], l[i1]
+            i1 += 1
+            i2 += 1
+        if not swap:
+            break
 
 
 if __name__ == '__main__':
-    rl = list(range(-10, 50))
+    #rl = list(range(-10, 50))
+    #shuffle(rl)
+    #print(rl)
+    #print(merge_sort(rl))
+
+    rl = list(range(-10, 100))
     shuffle(rl)
+    bubble_sort(rl)
     print(rl)
-    print(merge_sort_recursive(rl))
